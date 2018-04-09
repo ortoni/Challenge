0b4edbef978d8e7e80da934cf3627ea5f4cd62a3
koushik <koushik@koushik-PC>
2018-04-09 21:23:15 +0530

    don

--- src/main/java/challenge/DownloadFiles.java
+++ src/main/java/challenge/DownloadFiles.java
@@ -14,13 +14,20 @@ public class DownloadFiles {
 	@Test
 	public void downloadFiles()	{
 		options	 = new ChromeOptions();
-		prefs = new HashMap<String, Object>();	// To put preferences using "prefs" (KeyWord)
-		prefs.put("download.default_directory", downloadFilepath); //To set path
-		prefs.put("safebrowsing.enabled", "false"); // To disable security check (keep or Cancel)
-        options.addArguments("plugins.plugins_disabled", "Chrome PDF Viewer"); //to download PDF
-		options.setExperimentalOption("prefs", prefs); // Adding preferences to ChromeOptions
-		options.addArguments("--disable-extensions"); //to disable window browser
-		driver = new ChromeDriver(options);
+		// To put preferences using "prefs" (KeyWord)
+		prefs = new HashMap<String, Object>();	
+		//To set path
+		prefs.put("download.default_directory", downloadFilepath); 
+		// To disable security check (keep or Cancel)
+		prefs.put("safebrowsing.enabled", "false"); 
+		//to download PDF, pdf plugin is turning off
+		options.addArguments("plugins.plugins_disabled", "Chrome PDF Viewer"); 
+		// Adding preferences to ChromeOptions
+		options.setExperimentalOption("prefs", prefs); 
+		//to disable window browser
+		options.addArguments("--disable-extensions"); 
+		// Adding Chromeoptions to driverChromeD
+		driver = new ChromeDriver(options); 		
 		driver.manage().window().maximize();
 		driver.get("https://www.win-rar.com/predownload.html");
 		driver.findElementByLinkText("Download WinRAR").click();
