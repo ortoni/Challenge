cc1306a114b61fbf6f8898796ff904bac3e370ff
Koushik <koushik350@gmail.com>
2018-05-14 18:50:25 +0530

    Challenge Updated

--- src/main/java/challenge/ScrollDown.java
+++ src/main/java/challenge/ScrollDown.java
@@ -2,23 +2,16 @@ package challenge;
 
 import java.awt.AWTException;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class ScrollDown {
 
 	public static void main(String[] args) throws InterruptedException, AWTException {
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.naukri.com/");
-
 		int y = driver.findElementByLinkText("Browse All Jobs").getLocation().getY();
-
-		((JavascriptExecutor) driver).executeScript("scroll(0,"+y+");");
+		driver.executeScript("scroll(0,"+y+");", "");
 		
 	}
 }
--- src/main/java/challenge/ZoomPage.java
+++ src/main/java/challenge/ZoomPage.java
@@ -5,29 +5,34 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ZoomPage {
-	ChromeDriver d ;
+	ChromeDriver driver;
+
 	@Test
 	public void main() throws IOException {
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		d = new ChromeDriver();
-		d.manage().window().maximize();
-		d.get("https://jqueryui.com/draggable/");
+		// System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/draggable/");
+		// WebElement drag = d.findElementByLinkText("Draggable");
 		takeSnap();
-		d.switchTo().frame(0);
-		d.executeScript("document.body.style.zoom='170%'", "");
+		driver.switchTo().frame(0);
+		driver.executeScript("document.body.style.zoom='170%'", "");
 		takeSnap();
-		d.quit();
+		// d.quit();
 	}
-	int i =1;
+
+	int i = 1;
+
 	public long takeSnap() throws IOException {
 
-		File src = d.getScreenshotAs(OutputType.FILE);
-		
-		File dest = new File("./snap/ZoomPage"+i+".jpg");
+		File src = driver.getScreenshotAs(OutputType.FILE);
+
+		File dest = new File("./snap/ZoomPage" + i + ".jpg");
 		try {
 			FileUtils.copyFile(src, dest);
 		} catch (Exception e) {
