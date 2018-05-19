2308fb4c0d5e72ce02fd4fc95978d81054d077f3
Koushik <koushik350@gmail.com>
2018-05-19 10:59:24 +0530

    Region Snap

---
+++ src/main/java/challenge/RegionSnap.java
@@ -0,0 +1,43 @@
+package challenge;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class RegionSnap {
+ChromeDriver driver;
+@Test
+	public void main() throws IOException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		/*WebElement button = driver.findElementByClassName("decorativeSubmit");
+		elementScreenshot(button);*/
+		
+		WebElement div = driver.findElementByXPath("//form[@id='login']");
+		elementScreenshot(div);
+		driver.quit();
+	}
+
+	public void elementScreenshot(WebElement ele) throws IOException {
+		File screenshotAs = driver.getScreenshotAs(OutputType.FILE);
+		int x = ele.getLocation().getX();
+		int y = ele.getLocation().getY();
+		int width = ele.getSize().getWidth();
+		int height = ele.getSize().getHeight();
+		BufferedImage img = ImageIO.read(screenshotAs);
+		BufferedImage subimage = img.getSubimage(x, y, width, height);
+		ImageIO.write(subimage, "png", new File("./Snap/cropped.png"));
+	}
+
+}
