65c2d0aad52902a214b54e01200b8585a5d073e0
Koushik <koushik350@gmail.com>
2018-05-21 17:40:49 +0530

    Crop Element

--- src/main/java/challenge/ChromeDemo.java
+++ src/main/java/challenge/ChromeDemo.java
@@ -6,30 +6,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class ChromeDemo {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
 		ChromeOptions op = new ChromeOptions();
 		op.addArguments("--disable-notifications");
+		op.addArguments("--allow-silent-push");
 		ChromeDriver driver = new ChromeDriver(op);
 
-		
-
-		// Headless mode
-		//ch.setHeadless(true);
-
-		//To run in private mode
-		//ch.addArguments("-incognito");
-
-		//To disable automation browser running infobar
-		//ch.addArguments("disable-infobars");
-
-		//To maximize the browser
-		//ch.addArguments("start-maximized");
-
-		//To run the browser in full screen
-	
-
-		driver.get("http://www.google.com");
-		System.out.println(driver.getTitle());
+		driver.get("https://www.google.co.in/");
+		driver.findElementById("gs_st0").click();
+		//System.out.println(driver.getTitle());
+		//driver.switchTo().alert().accept();
 	}
 
 }
--- src/main/java/challenge/ElementSnap.java
+++ src/main/java/challenge/ElementSnap.java
@@ -11,13 +11,17 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class ElementSnap {
-ChromeDriver driver;
-@Test
+	ChromeDriver driver;
+	ChromeOptions options;
+	@Test
 	public void main() throws IOException {
-		driver = new ChromeDriver();
+		options = new ChromeOptions();
+		options.setHeadless(true);
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("http://leaftaps.com/opentaps/control/main");
@@ -27,11 +31,14 @@ ChromeDriver driver;
 	}
 
 	public void elementScreenshot(WebElement ele) throws IOException {
+		// Take Snap first
 		File screenshotAs = driver.getScreenshotAs(OutputType.FILE);
 		int x = ele.getLocation().getX();
 		int y = ele.getLocation().getY();
 		int width = ele.getSize().getWidth();
 		int height = ele.getSize().getHeight();
+
+		//After getting x, y, h & w, crop the image using BufferedImage - getSubimage
 		BufferedImage img = ImageIO.read(screenshotAs);
 		BufferedImage subimage = img.getSubimage(x, y, width, height);
 		ImageIO.write(subimage, "png", new File("./Snap/cropped.png"));
