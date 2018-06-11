e1ae9fb7c0d38c891e6d484063175a0ec9dd265e
Koushik <koushik350@gmail.com>
2018-06-11 16:52:26 +0530

    chromeDemo

--- src/main/java/challenge/ChromeDemo.java
+++ src/main/java/challenge/ChromeDemo.java
@@ -8,12 +8,13 @@ public class ChromeDemo {
 	public static void main(String[] args) {
 		//System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
 		ChromeOptions op = new ChromeOptions();
-		op.addArguments("--disable-notifications");
-		op.addArguments("--allow-silent-push");
+//		op.addArguments("--disable-notifications");
+//		op.addArguments("--allow-silent-push");
+		op.addArguments("--disable-translate-new-ux");
 		ChromeDriver driver = new ChromeDriver(op);
 
 		driver.get("https://www.google.co.in/");
-		driver.findElementById("gs_st0").click();
+		//driver.findElementById("gs_st0").click();
 		//System.out.println(driver.getTitle());
 		//driver.switchTo().alert().accept();
 	}
--- src/main/java/challenge/UploadFileUsingFileDetector.java
+++ src/main/java/challenge/UploadFileUsingFileDetector.java
@@ -4,21 +4,24 @@ import java.awt.AWTException;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebElement;
 
 public class UploadFileUsingFileDetector {
 	public static void main(String[] args) throws InterruptedException, AWTException {
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		InternetExplorerDriver
+		driver = new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.naukri.com/");
 		WebElement ele = driver.findElementByXPath("//input[@type='file']");
-		String path = "./Koushik Chatterjee_Automation(Selenium).pdf";
-		LocalFileDetector detect = new LocalFileDetector();
-		((RemoteWebElement)ele).setFileDetector(detect);
-		ele.sendKeys(detect.getLocalFile(path).getAbsolutePath());
-		Thread.sleep(10000);
-		driver.quit();
+		String path = "K:\\Koushik\\workspace\\challenge\\Koushik Chatterjee_Automation(Selenium).pdf";
+		ele.sendKeys(path);
+//		LocalFileDetector detect = new LocalFileDetector();
+//		((RemoteWebElement)ele).setFileDetector(detect);
+//		ele.sendKeys(detect.getLocalFile(path).getAbsolutePath());
+//		Thread.sleep(10000);
+//		driver.quit();
 	}
 }
