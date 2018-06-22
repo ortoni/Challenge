b3290b475976003d917d2b5a18548b8e083e7f9b
Koushik <koushik350@gmail.com>
2018-06-22 12:15:16 +0530

    failure

--- src/main/java/challenge/UploadFileUsingFileDetector.java
+++ src/main/java/challenge/UploadFileUsingFileDetector.java
@@ -1,27 +1,57 @@
 package challenge;
 
-import java.awt.AWTException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.Test;
 
 public class UploadFileUsingFileDetector {
-	public static void main(String[] args) throws InterruptedException, AWTException {
-		//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		InternetExplorerDriver
-		driver = new InternetExplorerDriver();
+	@Test
+	public void upload() throws MalformedURLException{
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("platform", "Windows 10");
+		caps.setCapability("version", "67.0");
+
+		/*http://uesrId:AccessId@ondemand.saucelabs.com:80/wd/hub
+		Access ID: c35b5eee-95d7-4a73-8be2-d05b77456594
+		 */
+		RemoteWebDriver driver = new RemoteWebDriver(new URL
+				("http://TunaTestDemo:c35b5eee-95d7-4a73-8be2-d05b77456594@ondemand.saucelabs.com:80/wd/hub"), caps);
+		
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// Load the URL
 		driver.get("https://www.naukri.com/");
-		WebElement ele = driver.findElementByXPath("//input[@type='file']");
-		String path = "K:\\Koushik\\workspace\\challenge\\Koushik Chatterjee_Automation(Selenium).pdf";
-		ele.sendKeys(path);
-//		LocalFileDetector detect = new LocalFileDetector();
-//		((RemoteWebElement)ele).setFileDetector(detect);
-//		ele.sendKeys(detect.getLocalFile(path).getAbsolutePath());
-//		Thread.sleep(10000);
-//		driver.quit();
+
+		// Locate the WebElement
+		WebElement upload = driver.findElementByXPath("//input[@type='file']");
+
+		LocalFileDetector detector = new LocalFileDetector();
+		((RemoteWebElement)upload).setFileDetector(detector);
+		upload.sendKeys(detector.getLocalFile("./Resume.pdf").getAbsolutePath());
+
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
